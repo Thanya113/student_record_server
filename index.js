f7c68eb909c2b7ad340b4ee50dd704928ee6234c
Thanya113 <118598797+Thanya113@users.noreply.github.com>
2023-10-30 17:35:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 8081
-const mongoURI = 'mongodb+srv://thanyarg21it:thanyamongo@cluster0.1ufufbq.mongodb.net/crudoperation';
-const facultyMongoURI = 'mongodb+srv://thanyarg21it:thanyamongo@cluster0.1ufufbq.mongodb.net/facultyoperation';
+const mongoURI = 'mongodb+srv://thanyarg21it:Thanyamongo@cluster0.1ufufbq.mongodb.net/crudoperation';
+const facultyMongoURI = 'mongodb+srv://thanyarg21it:Thanyamongo@cluster0.1ufufbq.mongodb.net/facultyoperation';
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
